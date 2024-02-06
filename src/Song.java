57d31d62c1f064f8004742010f7830c3742703b6
felipepereira2697 <felipepereira2697@gmail.com>
2024-02-06 22:40:41 +0000

    100 Days of Code - Java - Head First using Enhanced for

--- src/Song.java
+++ src/Song.java
@@ -3,6 +3,7 @@ public class Song {
     //Instance variables: things the object KNOWS
     //Encapsulation plays a big part here to avoid code to be add anything that is inappropriate.
     //We force this making the rest of the code go through setter methods and validate the parameter.
+    //Instance variables are declared inside a class but no within a method.
     private String title;
     private String artist;
 
@@ -25,7 +26,10 @@ public class Song {
 
     //    Things that an object DOES
     public String play() {
-        return "Now playing "+this.title+" from "+this.artist;
+        //Local variables do not get a default value, please initialize them.
+        //for this example, I'll keep in that way.
+        String musicDetails = "Now playing "+this.title+" from "+this.artist;
+        return musicDetails;
     }
     public String getTitle() {
         return this.title;
--- src/SongMainClass.java
+++ src/SongMainClass.java
@@ -1,11 +1,25 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SongMainClass {
     public static void main(String[] args)  {
         Song am = new Song();
         am.setArtist("Arctic Monkeys");
         am.setTitle("505");
-        System.out.println(am.play());
+        Song beatles = new Song();
+        beatles.setArtist("The Beatles");
+        beatles.setTitle("The Long and Winding Road");
+        List<Song> songList = new ArrayList<Song>();
+        songList.add(am);
+        songList.add(beatles);
+
+        //Using Enhanced for
+        for(Song item : songList) {
+            System.out.println(item.play());
+        }
+
+
 
 
     }
